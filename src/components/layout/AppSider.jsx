be60370258a85b21ca0771248d0f32ddd80be85f
roma_marshall <roma_marshall@outlook.com>
2024-06-23 14:37:16 +0200

    Card extended

--- src/components/layout/AppSider.jsx
+++ src/components/layout/AppSider.jsx
@@ -1,20 +1,12 @@
-import React, { useEffect, useState } from 'react'
-import { Layout, Card, Statistic, List, Typography, Spin } from 'antd'
-import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons'
-import { fakeFetchAssets, fakeFetchCrypto } from '../../api'
-import { percentDifference } from '../../utils'
+import React, {useEffect, useState} from 'react'
+import {Layout, Card, Statistic, List, Typography, Spin, Tag} from 'antd'
+import {ArrowDownOutlined, ArrowUpOutlined} from '@ant-design/icons'
+import {fakeFetchAssets, fakeFetchCrypto} from '../../api'
+import {percentDifference} from '../../utils'
 
 const siderStyle = {
   padding: '1rem',
 }
-
-const data = [
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.',
-]
 
 export default function AppSider() {
   const [loading, setLoading] = useState(false)
@@ -24,7 +16,7 @@ export default function AppSider() {
   useEffect(() => {
     async function preload() {
       setLoading(true)
-      const { result } = await fakeFetchCrypto()
+      const {result} = await fakeFetchCrypto()
       const assets = await fakeFetchAssets()
 
       setCrypto(result)
@@ -40,32 +32,50 @@ export default function AppSider() {
       }))
       setLoading(false)
     }
+
     preload()
   }, [])
 
   if (loading) {
-    return <Spin fullscreen />
+    return <Spin fullscreen/>
   }
 
 
   return (
     <Layout.Sider width="25%" style={siderStyle}>
       {assets.map(asset => (
-        <Card key={asset.id} style={{ marginBottom: '1rem' }}>
+        <Card key={asset.id} style={{marginBottom: '1rem'}}>
           <Statistic
             title={asset.id}
             value={asset.totalAmount}
             precision={2}
-            valueStyle={{ color: asset.grow ? '#3f8600' : '#cf1322' }}
-            prefix={asset.grow ? <ArrowUpOutlined /> : <ArrowDownOutlined />}
+            valueStyle={{color: asset.grow ? '#3f8600' : '#cf1322'}}
+            prefix={asset.grow ? <ArrowUpOutlined/> : <ArrowDownOutlined/>}
             suffix="$"
           />
           <List
             size="small"
-            dataSource={data}
+            dataSource={[
+              {title: 'Total Profit', value: asset.totalProfit, withTag: true},
+              {title: 'Asset Amount', value: asset.amount, isPlain: true},
+              // {title: 'Difference', value: asset.growPercent},
+            ]}
             renderItem={(item) => (
               <List.Item>
-                <Typography.Text mark>[ITEM]</Typography.Text> {item}
+                <span>{item.title}</span>
+                <span>
+                  {item.withTag && (
+                    <Tag color={asset.grow ? 'green' : 'red'}>
+                      {asset.growPercent}%
+                    </Tag>
+                  )}
+                  {item.isPlain && item.value}
+                    {!item.isPlain && (
+                      <Typography.Text type={asset.grow ? 'success' : 'danger'}>
+                        {item.value.toFixed(2)}$
+                      </Typography.Text>
+                    )}
+                </span>
               </List.Item>
             )}
           />
